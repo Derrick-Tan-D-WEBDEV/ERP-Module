0b6cb165eca869f908e7e7878aecdf44f17e9bc5
Derrick Tan D-WEBDEV <derricktpl@derrickwebdev.com>
2021-04-17 18:01:01 +0800

    commit

---
+++ src/app/pages/app-sc/user/add-standard-part-ms/add-standard-part-ms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-standard-part-ms',
+  templateUrl: './add-standard-part-ms.component.html',
+  styleUrls: ['./add-standard-part-ms.component.scss']
+})
+export class AddStandardPartMsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
