5aafcf8ec4fa95297f7cf8a888ac4166724e3728
Derrick Tan D-WEBDEV <derricktpl@derrickwebdev.com>
2021-04-21 15:01:25 +0800

    commit

--- src/app/pages/app-sc/user/add-standard-part/add-standard-part.component.ts
+++ src/app/pages/app-sc/user/add-standard-part/add-standard-part.component.ts
@@ -17,6 +17,7 @@ import { StandardPartCategoryService } from 'src/app/services/standard-part-cate
 export class AddStandardPartComponent implements OnInit {
   sp_category:any;
   addForm: FormGroup;
+  isSubmitted:boolean;
   constructor(private _standardPartService:StandardPartsService,private _chRef: ChangeDetectorRef,private _standardPartCategoryService:StandardPartCategoryService,private _authService: AuthService,private router:Router,private formBuilder:FormBuilder,private _toastrService: ToastrService) { }
 
   ngOnInit() {
@@ -31,7 +32,8 @@ export class AddStandardPartComponent implements OnInit {
       uom: ['', Validators.required],
       remark: ['', Validators.required],
       assign_material: ['', Validators.required],
-      assign_weight: ['', Validators.required]
+      assign_weight: ['', Validators.required],
+      folder_location: ['', Validators.required]
     });
   }
 
@@ -40,27 +42,68 @@ export class AddStandardPartComponent implements OnInit {
   }
 
   add(values){
+    this.isSubmitted = true;
     values["user_id"] = this._authService.getUserID();
     console.log(values);
-    this._standardPartService.addSP(values).subscribe((response) => {
-      console.log(response);
-    },
-    error => {
-      this._toastrService.show(
-        '<span class="alert-icon ni ni-bell-55" data-notify="icon"></span> <div class="alert-text"</div> <span class="alert-title" data-notify="title">Fail to retrieve category!</span> <span data-notify="message">Please contact the support team, if needed!</span></div>',
-        "",
-        {
-          timeOut: 1000,
-          closeButton: true,
-          enableHtml: true,
-          tapToDismiss: false,
-          titleClass: "alert-title",
-          positionClass: "toast-bottom-center",
-          toastClass:
-            "ngx-toastr alert alert-dismissible alert-danger alert-notify"
+    if(this.addForm.invalid){
+      return;
+    }
+    else{
+      this._standardPartService.addSP(values).subscribe((response) => {
+        console.log(response);
+        if(response.status){
+          this._toastrService.show(
+            '<span class="alert-icon ni ni-bell-55" data-notify="icon"></span> <div class="alert-text"</div> <span class="alert-title" data-notify="title">Successfully add standard parts!</span> <span data-notify="message">Nice!</span></div>',
+            "",
+            {
+              timeOut: 1000,
+              closeButton: true,
+              enableHtml: true,
+              tapToDismiss: false,
+              titleClass: "alert-title",
+              positionClass: "toast-bottom-center",
+              toastClass:
+                "ngx-toastr alert alert-dismissible alert-success alert-notify"
+            }
+          );      
+          this.addForm.reset();    
+          this.isSubmitted = false;
         }
-      );
-    });
+        else{
+          this._toastrService.show(
+            '<span class="alert-icon ni ni-bell-55" data-notify="icon"></span> <div class="alert-text"</div> <span class="alert-title" data-notify="title">'+response.message+'</span> <span data-notify="message">Please contact the support team, if needed!</span></div>',
+            "",
+            {
+              timeOut: 1000,
+              closeButton: true,
+              enableHtml: true,
+              tapToDismiss: false,
+              titleClass: "alert-title",
+              positionClass: "toast-bottom-center",
+              toastClass:
+                "ngx-toastr alert alert-dismissible alert-danger alert-notify"
+            }
+          );
+        }
+      },
+      error => {
+        this._toastrService.show(
+          '<span class="alert-icon ni ni-bell-55" data-notify="icon"></span> <div class="alert-text"</div> <span class="alert-title" data-notify="title">Fail to add standard parts!</span> <span data-notify="message">Please contact the support team, if needed!</span></div>',
+          "",
+          {
+            timeOut: 1000,
+            closeButton: true,
+            enableHtml: true,
+            tapToDismiss: false,
+            titleClass: "alert-title",
+            positionClass: "toast-bottom-center",
+            toastClass:
+              "ngx-toastr alert alert-dismissible alert-danger alert-notify"
+          }
+        );
+      });
+    }
+
   }
 
   getAllSPCategory(){
